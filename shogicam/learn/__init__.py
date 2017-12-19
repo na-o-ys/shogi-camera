682c81e50cec64458883851855723c9a52c487be
naoyoshi.okamae <naoyoshi.okamae@livesense.co.jp>
2017-12-19 12:32:25 +0900

    wip

--- shogicam/learn/__init__.py
+++ shogicam/learn/__init__.py
@@ -3,12 +3,14 @@ from ._purple import learn as purple
 from ._purple2 import learn as purple2
 from ._blue import learn as blue
 from ._blue2 import learn as blue2
+from ._yellow import learn as yellow
 
 models = {
    "purple": purple,
    "purple2": purple2,
    "blue": blue,
-   "blue2": blue2
+   "blue2": blue2,
+   "yellow": yellow
 }
 
 def learn_model(name, data_dir, verbose=True):
